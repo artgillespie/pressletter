d3341ec477717d8725da231c67f2366b2d5284f4
Art Gillespie <art@tapsquare.com>
2013-08-07 12:49:47 -0700

    add GPL license

--- Pressletter/ALGAppDelegate.m
+++ Pressletter/ALGAppDelegate.m
@@ -2,9 +2,20 @@
 //  ALGAppDelegate.m
 //  Pressletter
 //
-//  Created by Art Gillespie on 11/2/12.
-//  Copyright (c) 2012 Art Gillespie. All rights reserved.
+//  Copyright (C) 2012, 2013  Art Gillespie
 //
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.//
 
 #import "ALGAppDelegate.h"
 
